69bb174f5d6b3d529f4f30053a82b8a9fb746b2d
Ranizt <chicuongvo3@gmail.com>
2024-11-15 18:11:31 +0700

    Import @mui/material

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,36 +1,12 @@
 // import { useState } from "react";
-import NavBar from "../components/NavBar";
-import Hero from "../components/Hero";
-import FeaturedIn from "../components/FeaturedIn";
-import HowItWorks from "../components/HowItWorks";
-import CoreProblem from "../components/CoreProblem.tsx";
-import Archive from "../components/Archive.tsx";
-import Story from "../components/Story.tsx";
-import Plan from "../components/Plan.tsx";
-import Question from "../components/Question.tsx";
-import EndPage from "../components/EndPage.tsx";
+import MovieDetail from "./MovieDetail/MovieDetail.tsx";
 
 function App() {
   return (
     <>
-      <NavBar></NavBar>
-      <LandingContainer>
-        <Hero></Hero>
-        <FeaturedIn></FeaturedIn>
-        <CoreProblem></CoreProblem>
-        <HowItWorks></HowItWorks>
-        <Archive></Archive>
-        <Story></Story>
-        <Plan></Plan>
-        <Question></Question>
-        <EndPage></EndPage>
-      </LandingContainer>
+      <MovieDetail />
     </>
   );
-}
-
-function LandingContainer({ children }: { children: React.ReactNode }) {
-  return <div className="container">{children}</div>;
 }
 
 export default App;
