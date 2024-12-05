986a3ce6a747baafe887b27ca3bda4ea015c1461
Ranizt <chicuongvo3@gmail.com>
2024-12-05 12:04:54 +0700

    Format code

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,11 +1,11 @@
-import {useLocation, useNavigate} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/userContext.tsx";
 import { useEffect } from "react";
 
 function ProtectedRoute() {
   const { username } = useUser();
   const navigate = useNavigate();
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (pathname === "/login" || pathname === "/signup") return;
